fd11d64e01b8ed0a8dd5b923a968882e6268bc73
Marcelo Vieira <marceloquinze@gmail.com>
2024-07-30 15:39:44 -0300

    added transactions component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,20 @@
-import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
+import * as Dialog from '@radix-ui/react-dialog'
 import logoImg from '../../assets/logo.svg'
+import { NewTransactionModal } from '../NewTransactionModal'
 
 export function Header() {
-	return (
-		<HeaderContainer>
-			<HeaderContent>
-				<img src={logoImg} alt="" />
-				<NewTransactionButton>New Transaction</NewTransactionButton>
-			</HeaderContent>
-		</HeaderContainer>
-	)
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} alt="" />
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>New Transaction</NewTransactionButton>
+          </Dialog.Trigger>
+          <NewTransactionModal />
+        </Dialog.Root>
+      </HeaderContent>
+    </HeaderContainer>
+  )
 }
