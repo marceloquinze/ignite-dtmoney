b33a6dae027e1b0ccc5cbc4da326a7e95554bf23
Marcelo Vieira <marceloquinze@gmail.com>
2024-08-02 17:06:46 -0300

    added: transactions reduce on Summary comp

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -6,9 +6,24 @@ import { TransactionsContext } from '../../contexts/TransactionsContext'
 export function Summary() {
   const { transactions } = useContext(TransactionsContext)
 
-  const total = transactions
-    .filter((item) => item.type === 'income')
-    .reduce((acc, curr) => acc + curr.price, 0)
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.price
+        acc.total += transaction.price
+      } else {
+        acc.outcome += transaction.price
+        acc.total -= transaction.price
+      }
+      return acc
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    },
+  )
+  // const income = summary.filter((item) => item.type === 'income')
 
   return (
     <SummaryContainer>
@@ -17,21 +32,21 @@ export function Summary() {
           <span>Incomes</span>
           <ArrowCircleUp size={32} color="#00B37E" />
         </header>
-        <strong>R$ {total}</strong>
+        <strong>R$ {summary.income}</strong>
       </SummaryCard>
       <SummaryCard>
         <header>
           <span>Outcomes</span>
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>R$ {summary.outcome}</strong>
       </SummaryCard>
       <SummaryCard variant="green">
         <header>
           <span>Total</span>
           <CurrencyDollar size={32} color="#fff" />
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>R$ {summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
